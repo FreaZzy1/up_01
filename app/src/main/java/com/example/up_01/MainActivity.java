b9a16ed01e1b631c332a50376880d009dfe74850
artem <cheremnykh1707@mail.ru>
2025-04-17 22:30:01 +0500

    все готово.Черемных

--- app/src/main/java/com/example/up_01/MainActivity.java
+++ app/src/main/java/com/example/up_01/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.up_01;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +15,50 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Timer;
 
+public class MainActivity extends AppCompatActivity {
+    ProgressBar progressBar;
+    Timer timer;
+    int seconds;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        progressBar = findViewById(R.id.progressbar);
+        onBoardingScreen_1();
+    }
+
+    public boolean isInernetAvailable(Context context){
+        ConnectivityManager connectivityManager = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    public void onBoardingScreen_1(){
+        if(isInernetAvailable(this) == true){
+            startTimer();
+        }
+        else{
+            progressBar.setProgress(0);
+            progressBar.setVisibility(View.GONE);
+            startTimer();
+        }
+    }
+
+    private void startTimer(){
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, onBoardingScreen_1.class);
+                startActivity(intent);
+            }
+        }, 2000);
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        if(timer != null) timer.cancel();
     }
 }
